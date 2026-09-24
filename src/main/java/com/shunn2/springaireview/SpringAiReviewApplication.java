c12529035f477155e560e9d0d38fcaa6588a6d43
shunn2 <chsida655@gmail.com>
2026-09-24 18:47:20 +0900

    init: project setting

---
+++ src/main/java/com/shunn2/springaireview/SpringAiReviewApplication.java
@@ -0,0 +1,13 @@
+package com.shunn2.springaireview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shunn2/springaireview/SpringAiReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shunn2.springaireview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
